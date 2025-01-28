cbb50e2dfa62804609e672e7fb8fdf220dfa3256
Lincoln <linnsilva3636@gmail.com>
2025-01-28 04:29:54 -0300

    ConfigAws

--- demo/src/main/java/com/example/demo/controller/EmailSenderController.java
+++ demo/src/main/java/com/example/demo/controller/EmailSenderController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.application.EmailSenderService;
+import com.example.demo.core.EmailRequest;
+import com.example.demo.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +22,14 @@ public class EmailSenderController {
         this.emailSenderService = emailSenderService;
     }
 
-
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try{
+            this.emailSenderService.SendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email sent");
+        } catch (EmailServiceException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+        }
+    }
 
 }
---
+++ demo/src/main/java/com/example/demo/core/EmailRequest.java
@@ -0,0 +1,4 @@
+package com.example.demo.core;
+
+public record EmailRequest(String to, String subject, String body) {
+}
